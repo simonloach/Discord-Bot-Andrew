de9614e37c3f4f203d5fd4d6610212cc78e67968
konsprin <kossarpl@gmail.com>
2020-03-04 20:54:24 +0100

    First commit on my branch

--- andrzej.py
+++ andrzej.py
@@ -1,1 +1,19 @@
-print("pierwszy commit")
+# andrzej.py
+import os
+
+import discord
+
+# from dotenv import load_dotenv
+#       wyskakuje mi error przez to, ale w sumie nie jest to kompletnie potrzebne
+# load_dotenv()
+
+token = "Njg0MDYyMTMwMzg2ODk0OTcw.XmADZQ.McCDZLUUB7smn6SjvuagzjU8eZ8"   # os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(token)
