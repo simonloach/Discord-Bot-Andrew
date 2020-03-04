399b7383ba322bc9e9cf6bb8c74a883c7a580cdc
konsprin <kossarpl@gmail.com>
2020-03-04 21:03:52 +0100

    Connecting to the guild and reading members

--- andrzej.py
+++ andrzej.py
@@ -8,12 +8,22 @@ import discord
 # load_dotenv()
 
 token = "Njg0MDYyMTMwMzg2ODk0OTcw.XmADZQ.McCDZLUUB7smn6SjvuagzjU8eZ8"   # os.getenv('DISCORD_TOKEN')
+my_guild = "Server Andrzeja"
 
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == my_guild:
+            break
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 client.run(token)
